c4e398c195652c39124422989b4f8099f21233a8
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2025-05-18 17:08:27 +0700

    [learn-fun-kotlin] 03. Integer-Byte-Short-Long

---
+++ KotlinLes03IntegerType/src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main() {
+//    val name: String = "Phucdn"
+//    var age: Int = 25
+//    age = 35
+//
+//    println("Hello $name! Your age is $age")
+
+    var number: Int = 23
+    val integerMaxValue = Int.MAX_VALUE
+    val integerMinValue = Int.MIN_VALUE
+    println("Interger min value: $integerMinValue")
+    println("Interger max value: $integerMaxValue")
+//    number = 2147483648
+
+    // byte type
+    var byteMaxValue : Byte = Byte.MAX_VALUE
+    var byteMinValue : Byte = Byte.MIN_VALUE
+    println("Byte max value: $byteMaxValue")
+    println("Byte min value: $byteMinValue")
+
+    // short type:
+    var shortMaxValue: Short = Short.MAX_VALUE
+    var shortMinValue: Short = Short.MIN_VALUE
+    println("Short min value: $shortMinValue")
+    println("Short max value: $shortMaxValue")
+
+    // long type:
+    var longMaxValue : Long = Long.MAX_VALUE;
+    var longMinValue : Long = Long.MIN_VALUE;
+    println("Long min value: $longMinValue")
+    println("Long max value: $longMaxValue")
+}
