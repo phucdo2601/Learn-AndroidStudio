1a187cfe889d2a5f9afc2de3c660c58ca35ea63b
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2025-05-18 16:46:53 +0700

    [learn-fun-kotlin] 02. Variables

---
+++ KotlinLes02Variable/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var userName: String = "Phucdn"
+    userName = "John"
+    val physicalGender = "Male"
+//    physicalGender = "Female"
+
+    var age : Int = 25
+    age = 26
+//    age = true
+    println("Hello $userName your age is $age")
+}
