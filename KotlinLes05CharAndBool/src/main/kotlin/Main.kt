de6f2f77f0e5e122fbba2afef436f86f44f76dd4
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2025-05-18 17:28:58 +0700

    [learn-fun-kotlin] 05. Char and Boolean types

---
+++ KotlinLes05CharAndBool/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var aChar : Char = 'a'
+    var isDelete: Boolean? = null
+    println("The vaule of aChar: $aChar")
+    if (isDelete == true) {
+        println("The default of isDelete: true")
+
+    } else {
+        println("The default of isDelete: false")
+
+    }
+}
