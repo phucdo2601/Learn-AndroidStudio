319b64eefe1302f67eac7e7ec010db0d176bcb94
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2025-06-04 22:22:03 +0700

    [learn-fun-kotlin-b01] 06-01. Update operator b02

--- KotlinLes06Operators/src/main/kotlin/Main.kt
+++ KotlinLes06Operators/src/main/kotlin/Main.kt
@@ -4,33 +4,45 @@ fun main() {
     var result = x + y
 //    println("result = $result")
 
-    println("x + y = ${x + y}")
-    println("x - y = ${x - y}")
-    println("x * y = ${x * y}")
-    println("x / y = ${x / y}")
-    println("x % y = ${x % y}")
+//    println("x + y = ${x + y}")
+//    println("x - y = ${x - y}")
+//    println("x * y = ${x * y}")
+//    println("x / y = ${x / y}")
+//    println("x % y = ${x % y}")
+//
+//    result = 7 + 4
+//    println("result = $result")
+//    result += 9
+//    println("result = $result")
+//    result *= 3
+//    println("result = $result")
+//    result /= 2
+//    println("result = $result")
+//    result %= 7
+//    println("result = $result")
+//
+//    y = 3
+//    println("the value of y++: ${y++}")
+//    println("the value of ++y: ${++y}")
+//    println("the value of y--: ${y--}")
+//    println("the value of --y: ${--y}")
+//
+//    val isActive = true
+//    if (isActive) {
+//        println("The condition is true.")
+//    } else {
+//        println("The condition is false.")
+//    }
 
-    result = 7 + 4
-    println("result = $result")
-    result += 9
-    println("result = $result")
-    result *= 3
-    println("result = $result")
-    result /= 2
-    println("result = $result")
-    result %= 7
-    println("result = $result")
-
-    y = 3
-    println("the value of y++: ${y++}")
-    println("the value of ++y: ${++y}")
-    println("the value of y--: ${y--}")
-    println("the value of --y: ${--y}")
-
-    val isActive = true
-    if (isActive) {
-        println("The condition is true.")
+    var num1 = 5;
+    var num2 = -3;
+    var testVar01 = if (num1 > 0 || num2 > 0) {
+        println("Case01");
+        "Test-pri-method-b01";
     } else {
-        println("The condition is false.")
+        println("Case02");
+        "Test-pri-method-b02";
     }
+
+    println("$testVar01");
 }
